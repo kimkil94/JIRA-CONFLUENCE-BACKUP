62838eb567f2d5c997d78f2143790b2aaf4cd5c3
root <root@atlassian-dev.atlassian.lan>
2017-02-13 11:21:21 +0100

    added arguments handler, fixed some small bugs

--- jira_confluence_backup.sh
+++ jira_confluence_backup.sh
@@ -9,36 +9,6 @@ BACKUPDIR="/home/backup"
 MYSQLDUMPBASEFILENAME=$(date +%F)"_mysql_dump"
 PROJECT_NAME="TEST_PROJECT"
 ##-------------------------------------------------#
-
-
-##--------JIRA/CONFLUENCE SETTINGS---------#
-ATLASSIAN_BACKUP_FILE_NAME=${PROJECT_NAME}_atlassian-backup
-ATLASSIAN_BACKUP_DIR=$BACKUPDIR"/"$ATLASSIAN_BACKUP_FILE_NAME
-
-#Confluence
-#CONF_DB_USER=""
-#CONF_DB_PASSWD=""
-#CONF_DB_NAME=""
-
-#CONF_HOME=""
-#MYSQLDUMP_EXTENSION_CONFL=$MYSQLDUMPBASEFILENAME"_confluence.sql"
-
-#Matsuko jira settings
-JIRADBUSER="jirauser"
-JIRADBPASSWD="alofok"
-JIRADBNAME="jiradb"
-
-JIRA_HOME="/var/atlassian/application-data/jira/"
-JIRA_INSTALL_DIR="/opt/atlassian/jira/"
-
-MYSQLDUMP_EXTENSION_JIRA=$MYSQLDUMPBASEFILENAME"_"$PROJECT_NAME"_jira.sql"
-
-#Final compressed file name
-COMPRESSED_FILE_NAME=${ATLASSIAN_BACKUP_DIR}"/"$(date +%F)"_"${ATLASSIAN_BACKUP_FILE_NAME}.tar
-STAT_FILE="/var/stat_backup_file_${PROJECT_NAME}"
-NOSTAT="0"
-##-------------------------------------------------#
-
 ##-----Logging functions---------------------------#
 
 readonly SCRIPT_NAME=$(basename $0)
@@ -68,9 +38,77 @@ logmsg()
   logger -t $SCRIPT_NAME $IN
 }
 
+##--------------------------------------------------#
 
+
+##----------GetOpt----------------------------------#
+if [ $# -eq 0 ]; then
+        echo "No argument passed!"
+        echo "Usage $0 --database DB_NAME --username USERNAME --password PASSWORD"
+        exit 1
+fi
+
+
+
+TEMP=`getopt -o ab:c:: --long database:,username:,password: \
+     -n 'example.bash' -- "$@"`
+
+if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
+
+# Note the quotes around `$TEMP': they are essential!
+eval set -- "$TEMP"
+
+while true ; do
+        case "$1" in
+                #pass name of database
+                --database)  
+                        DBNAME=$2 
+                        shift 2 ;;
+                #pass username for accessing database
+                --username) 
+                        DBUSERNAME=$2 
+                        shift 2 ;;
+                #pass password for username
+                --password)  
+                        DBPASSWORD=$2 
+                        shift 2 ;;
+                --) shift ; break ;;
+                *) echo "Internal error!" ; exit 1 ;;
+        esac
+done
+
+##--------------------------------------------------#
+
+##--------JIRA/CONFLUENCE SETTINGS---------#
+ATLASSIAN_BACKUP_FILE_NAME=${PROJECT_NAME}_atlassian-backup
+ATLASSIAN_BACKUP_DIR=$BACKUPDIR"/"$ATLASSIAN_BACKUP_FILE_NAME
+
+#Confluence
+#CONF_DB_USER=""
+#CONF_DB_PASSWD=""
+#CONF_DB_NAME=""
+
+#CONF_HOME=""
+#MYSQLDUMP_EXTENSION_CONFL=$MYSQLDUMPBASEFILENAME"_confluence.sql"
+
+#Jira settings
+if [ ! -z $DBUSERNAME ] || [ ! -z $DBPASSWORD ] || [ ! -z $DBNAME ];then
+JIRADBUSER="$DBUSERNAME"
+JIRADBPASSWD="$DBPASSWORD"
+JIRADBNAME="$DBNAME"
+else
+	echo "[ERROR]: Database credentials were not set properly! please check arguments! " | errlogmsg
+fi
+JIRA_HOME="/var/atlassian/application-data/jira/"
+JIRA_INSTALL_DIR="/opt/atlassian/jira/"
+
+MYSQLDUMP_EXTENSION_JIRA=$MYSQLDUMPBASEFILENAME"_"$PROJECT_NAME"_jira.sql"
+
+#Final compressed file name
+COMPRESSED_FILE_NAME=${ATLASSIAN_BACKUP_DIR}"/"$(date +%F)"_"${ATLASSIAN_BACKUP_FILE_NAME}.tar
+STAT_FILE="/var/stat_backup_file_${PROJECT_NAME}"
+NOSTAT="0"
 ##-------------------------------------------------#
-
 
 
 ##------Start of backup script--------------------##
@@ -155,7 +193,7 @@ if [ -s ${COMPRESSED_FILE_NAME}.gz ];then
 		if [[ "$ACTUAL_SIZE" -gt "$LAST_SIZE" ]] || [[ "$ACTUAL_SIZE" -eq "$LAST_SIZE"  ]]; then
 			echo "[INF]Backup of $PROJECT_NAME jira instance done successfully." |logmsg
 			echo "[INF]Compressed archive file has $ACTUAL_SIZE MB" | logmsg
-			exut 0
+			exit 0
 		else
 			echo "[ERR]: Backup of $PROJECT_NAME jira instance was unsuccessful!"  | errlogmsg
 			echo "[INF]: Compressed archive file has only $ACTUAL_SIZE MB!" | errlogmsg
