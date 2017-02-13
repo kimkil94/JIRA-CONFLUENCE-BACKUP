e2c0313fc73c34487521e5907bce5dabd2785094
root <root@atlassian-dev.atlassian.lan>
2017-02-13 13:31:54 +0100

    variables correction, added two additional parameters- '--jira-home-dir' and '--jira-install-dir'

--- jira_confluence_backup.sh
+++ jira_confluence_backup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 #Backup jira and confluence
+#
+
 
 #Variables definition
 
@@ -13,6 +15,7 @@ PROJECT_NAME="TEST_PROJECT"
 
 readonly SCRIPT_NAME=$(basename $0)
 
+#log only stderr to syslog
 errlogmsg()
 {
   if [ -n "$1" ]
@@ -27,6 +30,7 @@ fi
 IN=""
 }
 
+#log only stdout to syslog
 logmsg()
 {
   if [ -n "$1" ]
@@ -40,17 +44,18 @@ logmsg()
 
 ##--------------------------------------------------#
 
-
 ##----------GetOpt----------------------------------#
+
+#check if at least one argument has been passed
 if [ $# -eq 0 ]; then
         echo "No argument passed!"
-        echo "Usage $0 --database DB_NAME --username USERNAME --password PASSWORD"
+        echo "Usage $0 --database DB_NAME --username USERNAME --password PASSWORD --jira-home-dir JIRA_HOME_DIR --jira-install-dir JIRA_INSTALL_DIR"
         exit 1
 fi
 
 
 
-TEMP=`getopt -o ab:c:: --long database:,username:,password: \
+TEMP=`getopt -o ab:c:: --long database:,username:,password:,jira-home-dir:,jira-install-dir: \
      -n 'example.bash' -- "$@"`
 
 if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
@@ -72,6 +77,13 @@ while true ; do
                 --password)  
                         DBPASSWORD=$2 
                         shift 2 ;;
+                --jira-home-dir)  
+                        JIRAHOME=$2 
+                        shift 2 ;;
+                 --jira-install-dir)  
+                        JIRAINSTALLDIR=$2 
+                        shift 2 ;;
+ 
                 --) shift ; break ;;
                 *) echo "Internal error!" ; exit 1 ;;
         esac
@@ -81,7 +93,7 @@ done
 
 ##--------JIRA/CONFLUENCE SETTINGS---------#
 ATLASSIAN_BACKUP_FILE_NAME=${PROJECT_NAME}_atlassian-backup
-ATLASSIAN_BACKUP_DIR=$BACKUPDIR"/"$ATLASSIAN_BACKUP_FILE_NAME
+ATLASSIAN_BACKUP_DIR=${BACKUPDIR}"/"${ATLASSIAN_BACKUP_FILE_NAME}
 
 #Confluence
 #CONF_DB_USER=""
@@ -92,17 +104,31 @@ ATLASSIAN_BACKUP_DIR=$BACKUPDIR"/"$ATLASSIAN_BACKUP_FILE_NAME
 #MYSQLDUMP_EXTENSION_CONFL=$MYSQLDUMPBASEFILENAME"_confluence.sql"
 
 #Jira settings
-if [ ! -z $DBUSERNAME ] || [ ! -z $DBPASSWORD ] || [ ! -z $DBNAME ];then
-JIRADBUSER="$DBUSERNAME"
-JIRADBPASSWD="$DBPASSWORD"
-JIRADBNAME="$DBNAME"
+if [ ! -z ${DBUSERNAME} ] || [ ! -z ${DBPASSWORD} ] || [ ! -z ${DBNAME} ]; then
+	JIRADBUSER=${DBUSERNAME}
+	JIRADBPASSWD=${DBPASSWORD}
+	JIRADBNAME=${DBNAME}
 else
 	echo "[ERROR]: Database credentials were not set properly! please check arguments! " | errlogmsg
 fi
-JIRA_HOME="/var/atlassian/application-data/jira/"
-JIRA_INSTALL_DIR="/opt/atlassian/jira/"
 
-MYSQLDUMP_EXTENSION_JIRA=$MYSQLDUMPBASEFILENAME"_"$PROJECT_NAME"_jira.sql"
+if [ ! -z ${JIRAHOME} ] || [ ! -z ${JIRAINSTALLDIR} ]; then
+	JIRA_HOME=${JIRAHOME}
+	JIRA_INSTALL_DIR=${JIRAINSTALLDIR}
+elif [ ! -d ${JIRAHOME} ]; then 
+	echo "[ERROR]: Jira Home Directory: ${JIRAHOME} does not exist!" | errlogmsg
+	echo "[INF]: exiting 1" | errlogmsg
+	exit 1
+elif [ -d ${JIRAINSTALLDIR} ]; then 
+	echo "[ERROR]: Jira Install Directory: ${JIRAINSTALLDIR} does not exist!" | errlogmsg
+	echo "[INF]: exiting 1" | errlogmsg
+	exit 1
+else
+	echo "[ERROR]: Jira Home Directory or Jira Install Directory has not been specified: Quiting! " | errlogmsg
+	exit 1
+fi
+
+MYSQLDUMP_EXTENSION_JIRA=${MYSQLDUMPBASEFILENAME}"_"${PROJECT_NAME}"_jira.sql"
 
 #Final compressed file name
 COMPRESSED_FILE_NAME=${ATLASSIAN_BACKUP_DIR}"/"$(date +%F)"_"${ATLASSIAN_BACKUP_FILE_NAME}.tar
@@ -123,8 +149,8 @@ if [ ! -e ${ATLASSIAN_BACKUP_DIR}  ]; then
    mkdir ${ATLASSIAN_BACKUP_DIR}
 fi
 
-if [ ! -e $STAT_FILE ]; then
-   touch $STAT_FILE
+if [ ! -e ${STAT_FILE} ]; then
+   touch ${STAT_FILE}
    NOSTAT="1"
 fi
 
@@ -143,7 +169,7 @@ echo "Starting backuping ${PROJECT_NAME} JIRA application data." | logmsg
    # Dump jira database into file
   JIRA_MYSQL_DUMP_FILE_NAME=${ATLASSIAN_BACKUP_DIR}"/"${MYSQLDUMP_EXTENSION_JIRA}
 
-  mysqldump -u ${JIRADBUSER} -p${JIRADBPASSWD} ${JIRADBNAME} --max_allowed_packet=512M  > $JIRA_MYSQL_DUMP_FILE_NAME 2>&1 >/dev/null | errlogmsg
+  mysqldump -u ${JIRADBUSER} -p${JIRADBPASSWD} ${JIRADBNAME} --max_allowed_packet=512M  > ${JIRA_MYSQL_DUMP_FILE_NAME} 2>&1 >/dev/null | errlogmsg
 
   # Create backup from JIRA home directory, Jira database dump files
   tar rf  ${COMPRESSED_FILE_NAME} ${JIRA_INSTALL_DIR} ${JIRA_HOME} ${JIRA_MYSQL_DUMP_FILE_NAME} 2>&1 >/dev/null | errlogmsg
@@ -162,8 +188,8 @@ echo "Starting backuping ${PROJECT_NAME} JIRA application data." | logmsg
 #  echo "FLUSH TABLES WITH READ LOCK;"
 #
 #  # Dump _conflunence database into file
-#  CONFL_MYSQL_DUMP_FILE_NAME=$ATLASSIAN_BACKUP_DIR/$MYSQLDUMP_EXTENSION_CONFL
-#  mysqldump -u $CONF_DB_USER -p$CONF_DB_PASSWD $CONF_DB_NAME --max_allowed_packet=512M > $CONFL_MYSQL_DUMP_FILE_NAME
+#  CONFL_MYSQL_DUMP_FILE_NAME=${ATLASSIAN_BACKUP_DIR}/${MYSQLDUMP_EXTENSION_CONFL}
+#  mysqldump -u ${CONF_DB_USER} -p${CONF_DB_PASSWD} ${CONF_DB_NAME} --max_allowed_packet=512M > ${CONFL_MYSQL_DUMP_FILE_NAME}
 #
 #  # Create backup from JIRA home directory, CONFLUENCE home directory, and Confluence and Jira database dump files
 #  tar rf  ${COMPRESSED_FILE_NAME} ${CONF_HOME} ${CONFL_MYSQL_DUMP_FILE_NAME}
@@ -178,30 +204,30 @@ echo "Starting backuping ${PROJECT_NAME} JIRA application data." | logmsg
 
 
 # Compress data with gzip
-/bin/gzip -f $COMPRESSED_FILE_NAME 2>&1 >/dev/null | errlogmsg
+/bin/gzip -f ${COMPRESSED_FILE_NAME} 2>&1 >/dev/null | errlogmsg
 
 if [ -s ${COMPRESSED_FILE_NAME}.gz ];then
-	echo "Backup file: $COMPRESSED_FILE_NAME created successfully." | logmsg
+	echo "Backup file: ${COMPRESSED_FILE_NAME} created successfully." | logmsg
 	
 	if [ $NOSTAT -eq "1" ]; then
-		du -m ${COMPRESSED_FILE_NAME}.gz | awk '{print $1}' > $STAT_FILE
+		du -m ${COMPRESSED_FILE_NAME}.gz | awk '{print $1}' > ${STAT_FILE}
 	fi
-	if [ $NOSTAT -eq "0" ]; then
-		LAST_SIZE=$(cat $STAT_FILE)
+	if [ ${NOSTAT} -eq "0" ]; then
+		LAST_SIZE=$(cat ${STAT_FILE})
 		ACTUAL_SIZE=$(du -m  ${COMPRESSED_FILE_NAME}.gz | awk '{print $1}')
 		
-		if [[ "$ACTUAL_SIZE" -gt "$LAST_SIZE" ]] || [[ "$ACTUAL_SIZE" -eq "$LAST_SIZE"  ]]; then
-			echo "[INF]Backup of $PROJECT_NAME jira instance done successfully." |logmsg
-			echo "[INF]Compressed archive file has $ACTUAL_SIZE MB" | logmsg
+		if [[ "${ACTUAL_SIZE}" -gt "${LAST_SIZE}" ]] || [[ "${ACTUAL_SIZE}" -eq "${LAST_SIZE}"  ]]; then
+			echo "[INF]Backup of ${PROJECT_NAME} jira instance done successfully." |logmsg
+			echo "[INF]Compressed archive file has ${ACTUAL_SIZE} MB" | logmsg
 			exit 0
 		else
-			echo "[ERR]: Backup of $PROJECT_NAME jira instance was unsuccessful!"  | errlogmsg
-			echo "[INF]: Compressed archive file has only $ACTUAL_SIZE MB!" | errlogmsg
+			echo "[ERR]: Backup of ${PROJECT_NAME} jira instance was unsuccessful!"  | errlogmsg
+			echo "[INF]: Compressed archive file has only ${ACTUAL_SIZE} MB!" | errlogmsg
 			exit 1
 		fi
 	fi
 else
-	echo "[ERR]: Backup file $COMPRESSED_FILE_NAME was not created!" | errlogmsg
+	echo "[ERR]: Backup file ${COMPRESSED_FILE_NAME} was not created!" | errlogmsg
 	exit 1
 fi
 
